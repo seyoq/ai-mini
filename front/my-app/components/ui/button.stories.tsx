5e94a6696c533a8e4da1a89e1bad120fdcf11a8a
seyoq <charningjun@gmail.com>
2025-07-30 13:22:28 +0900

    ‘gogogo’

--- front/my-app/components/ui/button.stories.tsx
+++ front/my-app/components/ui/button.stories.tsx
@@ -1,17 +1,17 @@
-import {StoryObj, Meta} from '@storybook/react';
+// import {StoryObj, Meta} from '@storybook/react';
 
-import { Button } from './button';
+// import { Button } from './button';
 
-export default {
-  title: 'button',
-  component: Button,
-  args: {
+// export default {
+//   title: 'button',
+//   component: Button,
+//   args: {
     
-  },
-} as Meta<typeof Button>;
+//   },
+// } as Meta<typeof Button>;
 
-type Story = StoryObj<typeof Button>;
+// type Story = StoryObj<typeof Button>;
 
-export const Default: Story = {
-  args: {},
-};
+// export const Default: Story = {
+//   args: {},
+// };
